3a138f33d1df1c0b1727125c7cbf72ab5424fd59
Tuan Bui <150992870+tuanbui-17@users.noreply.github.com>
2025-02-14 21:54:07 +0700

    modify header (#2)

--- src/icons/MoonIcon/index.tsx
+++ src/icons/MoonIcon/index.tsx
@@ -4,22 +4,23 @@ export function MoonIcon(props: Readonly<IconProps>) {
   const { className, fill, width = "20", height = "20" } = props;
 
   return (
-    <svg
-      className={className}
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      width={width}
-      height={height}
-      fill={fill}
-      viewBox="0 0 24 24"
-    >
-      <path
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M12 21a9 9 0 0 1-.5-17.986V3c-.354.966-.5 1.911-.5 3a9 9 0 0 0 9 9c.239 0 .254.018.488 0A9.004 9.004 0 0 1 12 21Z"
-      />
-    </svg>
+    <div className={className}>
+      <svg
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        fill={fill}
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M12 21a9 9 0 0 1-.5-17.986V3c-.354.966-.5 1.911-.5 3a9 9 0 0 0 9 9c.239 0 .254.018.488 0A9.004 9.004 0 0 1 12 21Z"
+        />
+      </svg>
+    </div>
   );
 }
